8e107d79e2374ac9178e73727b7a72e013ac28b6
Alessiovinci87 <alessio.vinci87@gmail.com>
2023-06-26 16:05:20 +0200

    Add milestone 1

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+const teamMembers = [
+    {
+      nome: "Mario Rossi",
+      ruolo: "Sviluppatore",
+      foto: "mario.jpg"
+    },
+    {
+      nome: "Laura Bianchi",
+      ruolo: "Progettista",
+      foto: "laura.jpg"
+    },
+    {
+      nome: "Luigi Verdi",
+      ruolo: "Responsabile",
+      foto: "luigi.jpg"
+    }
+  ];
