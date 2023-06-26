269515c794eadcc69bb8e56a90f98b9edb9fb960
Alessiovinci87 <alessio.vinci87@gmail.com>
2023-06-26 16:09:39 +0200

    Add milestone 2

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,24 @@ const teamMembers = [
       ruolo: "Sviluppatore",
       foto: "mario.jpg"
     },
+
     {
       nome: "Laura Bianchi",
       ruolo: "Progettista",
       foto: "laura.jpg"
     },
+    
     {
       nome: "Luigi Verdi",
       ruolo: "Responsabile",
       foto: "luigi.jpg"
     }
   ];
+
+  for (let i = 0; i < teamMembers.length; i++) {
+    const membro = teamMembers[i];
+    console.log("Nome: " + membro.nome);
+    console.log("Ruolo: " + membro.ruolo);
+    console.log("Foto: " + membro.foto);
+    console.log("-----------------------");
+  }
